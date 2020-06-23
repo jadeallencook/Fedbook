c510376da56510e32191cc8488202b485ee7cef6
Jade Allen Cook <jadeallencook@gmail.com>
2020-06-23 12:43:37 -0700

    moved assets and fixed catagories page

--- src/components/Catagories.js
+++ src/components/Catagories.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import './Catagories.scss';
+import PoliceIcon from '../assets/icons/police.png';
+import CourtIcon from '../assets/icons/court.png';
+import OtherIcon from '../assets/icons/other.png';
+import PoliticiansIcon from '../assets/icons/politicians.png';
 
 export default function () {
   return (
     <ul className="Catagories">
       <li>
-        <img src="/assets/symbols/police.png" alt="" />
+        <img src={PoliceIcon} alt="Police Icon" />
         Police
       </li>
       <li>
-        <img src="/assets/symbols/judge.png" alt="" />Court</li>
+        <img src={CourtIcon} alt="Court Icon" />
+        Court
+      </li>
       <li>
-        <img src="/assets/symbols/politicians.png" alt="" />Politicians</li>
+        <img src={PoliticiansIcon} alt="Politicians Icon" />
+        Politicians
+      </li>
       <li>
-        <img src="/assets/symbols/other.png" alt="" />Other</li>
+        <img src={OtherIcon} alt="Other Court Icon" />
+        Other
+      </li>
     </ul>
   );
 }
