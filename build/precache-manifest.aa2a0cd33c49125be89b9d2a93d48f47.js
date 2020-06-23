bfa351981897c2c3c32e7091028dfe0c3b83f851
Jade Allen Cook <jadeallencook@gmail.com>
2020-06-23 10:13:12 -0700

    exported production build

---
+++ build/precache-manifest.aa2a0cd33c49125be89b9d2a93d48f47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b55224aa6e58fd1dc1d830e8249479b4",
+    "url": "/fedbook/build/index.html"
+  },
+  {
+    "revision": "1252e69be3c2cfbf5362",
+    "url": "/fedbook/build/static/css/main.5cbcb140.chunk.css"
+  },
+  {
+    "revision": "c629b0abdf2bdd9bba12",
+    "url": "/fedbook/build/static/js/2.f732483d.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/fedbook/build/static/js/2.f732483d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1252e69be3c2cfbf5362",
+    "url": "/fedbook/build/static/js/main.d399be75.chunk.js"
+  },
+  {
+    "revision": "2d3562a9ed0c2e8ace22",
+    "url": "/fedbook/build/static/js/runtime-main.0acdf9c5.js"
+  }
+]);
